16992cfbc57ae331240e1c3e60db03f402f35762
AmanMirza9898 <aman9811269898@gmail.com>
2026-02-03 12:18:19 +0530

    all fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,11 +6,12 @@ const nextConfig: NextConfig = {
   basePath: '/law',          // <-- Subfolder path
   assetPrefix: '/law/',      // <-- Ensures assets load correctly
 
-  trailingSlash: true,       // Avoids 404 issues on Hostinger
+  trailingSlash: true,
+  output: "standalone",     // Avoids 404 issues on Hostinger
   images: {
     unoptimized: true,       // Needed for static export if using next/image
   },
-  
+
   // ✅ Empty turbopack config to silence the warning
   turbopack: {},
 };
